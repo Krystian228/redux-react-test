1b582a10adb5395109925f79a5864172e3d9ab1b
Krystian <krystian@qunabu.com>
2019-11-18 03:57:28 +0100

    Updates

--- src/redux/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import episodesReducer from './episodes/reducers';
-import charactersReducer from './characters/reducers';
-
-export default combineReducers({
-    episodesReducer,
-    charactersReducer
-});
